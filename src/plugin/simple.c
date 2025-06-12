8d2929653815c4a5399adb0f27c7be3aa8b048ba
lipeaaraujo <felipearaujodff@gmail.com>
2025-06-12 20:01:27 -0300

    implementando jeroslow-wang

--- src/plugin/simple.c
+++ src/plugin/simple.c
@@ -7,28 +7,76 @@
 void PreProcessing(Form* form){
 }
 
-enum DecideState Decide(const Form* form)
-{
+// decide padrão
+// enum DecideState Decide(const Form* form)
+// {
 
-    ClauseNode* list = form->clauses;
-    while(list != NULL)
-    {
+//     ClauseNode* list = form->clauses;
+//     while(list != NULL)
+//     {
 
-        Clause *clause = list->clause;
+//         Clause *clause = list->clause;
 
-        for(int i = 0; i<clause->size; ++i)
-        {
-            LiteralId lit = clause->literals[i];
-            lit = ((lit > 0 )? lit : -lit);
-            if(getVarState(lit-1) == UNK)
-            {
-                insertDecisionLevel(lit-1, FALSE);
+//         for(int i = 0; i<clause->size; ++i)
+//         {
+//             LiteralId lit = clause->literals[i];
+//             lit = ((lit > 0 )? lit : -lit);
+//             if(getVarState(lit-1) == UNK)
+//             {
+//                 insertDecisionLevel(lit-1, FALSE);
 
-                return FOUND_VAR;
-            }
+//                 return FOUND_VAR;
+//             }
+//         }
+
+//         list = list->next;
+//     }
+
+//     return ALL_ASSIGNED;
+// }
+
+// jeroslaw-wang
+enum DecideState Decide(const Form* form) {
+    int bestVar = -1;
+    double bestScore = -1.0;
+    double bestVarNegScore = -1.0;
+    double bestVarPosScore = -1.0;
+
+    for (int v=0; v < form->numClauses; v++) {
+        if (getVarState(v) != UNK) continue;
+        double score = 0.0;
+        double posScore = 0.0;
+        double negScore = 0.0;
+
+        ClauseNode* node = form->literals[2*v];
+        while (node) {
+            posScore += 1.0 / (1 << node->clause->size);
+            node = node->next;
         }
 
-        list = list->next;
+        node = form->literals[2*v+1];
+        while (node) {
+            negScore += 1.0 / (1 << node->clause->size);
+            node = node->next;
+        }
+
+        score = posScore + negScore;
+
+        if (score > bestScore) {
+            bestScore = score;
+            bestVar = v;
+            bestVarNegScore = negScore;
+            bestVarPosScore = posScore;
+        }
+    }
+
+    if (bestVar != -1) {
+        if (bestVarNegScore > bestVarPosScore) {
+            insertDecisionLevel(bestVar, FALSE);
+        } else {
+            insertDecisionLevel(bestVar, TRUE);
+        }
+        return FOUND_VAR;
     }
 
     return ALL_ASSIGNED;
